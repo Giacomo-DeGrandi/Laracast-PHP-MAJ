69834c6e5826c76b63eee959b19230c7d24e596e
giacomo-degrandi <carlo.de-grandi-giacomo@laplateforme.io>
2023-07-22 10:00:13 +0200

    commit beginning note project

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$business = [
+
+  'name' => 'DigiDev',
+  'cost' => 15,
+  'categories' => ['Development', 'Business', 'Design']
+];
+
+
+function register($user){
+
+  
+};
+
+require 'index.view.php';
+
+?>
