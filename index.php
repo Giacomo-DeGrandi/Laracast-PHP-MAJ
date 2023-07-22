9b192c2e8352741d772bf5a7c3b64e1e34568440
giacomo-degrandi <carlo.de-grandi-giacomo@laplateforme.io>
2023-07-22 20:17:33 +0200

    end router

--- index.php
+++ index.php
@@ -2,20 +2,6 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-if(array_key_exists($uri, $routes)){
-    require $routes[$uri];
-} else {
-    abort();
-}
-
-
+require 'router.php';
 
 ?>
